bc3271c271787d89546b36096d51000d3729d5fb
ayesha8221 <ayesha.glnt012@gmail.com>
2023-05-29 16:40:02 +0200

    ecommerce

---
+++ script/index.js
@@ -0,0 +1,41 @@
+// education array of objects
+
+let education = [
+    // {
+    //   id: 1,
+    //   year: 2020,
+    //   desc: "Latest certifacte",
+    //   place: "This place",
+    // },
+    // {
+    //   id: 2,
+    //   year: 2019,
+    //   desc: "Latest certifacte",
+    //   place: "This place",
+    // },
+    // {
+    //   id: 3,
+    //   year: 2018,
+    //   desc: "Latest certifacte",
+    //   place: "This place",
+    // },
+    // {
+    //   id: 4,
+    //   year: 2017,
+    //   desc: "Latest certifacte",
+    //   place: "This place",
+    // },
+    // {
+    //   id: 5,
+    //   year: 2016,
+    //   desc: "Latest certifacte",
+    //   place: "This place",
+    // },
+  ];
+  
+  let dispNavTop = document.getElementById("navbartop");
+  navbartop.forEach((data) => {
+    dispNavTop.innerHTML += `
+    
+`;
+  });
