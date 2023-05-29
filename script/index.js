04e7efe1bd107433ea43d2dc8c3ca8a8d871e0ec
Ayesha <ayesha.glnt012@gmail.com>
2023-05-30 00:06:57 +0200

    update

--- script/index.js
+++ script/index.js
@@ -1,41 +1,29 @@
-// education array of objects
+const navbarTop = [
+  {
+    id: navbarTop,
+    link1:  "./pages/index.html",
+    link2: "./pages/books.html",
+    link3: "./pages/about.html",
+    name1: "Home",
+    name2: "Buy Now",
+    name3: "About Us",
+  },
+];
 
-let education = [
-    // {
-    //   id: 1,
-    //   year: 2020,
-    //   desc: "Latest certifacte",
-    //   place: "This place",
-    // },
-    // {
-    //   id: 2,
-    //   year: 2019,
-    //   desc: "Latest certifacte",
-    //   place: "This place",
-    // },
-    // {
-    //   id: 3,
-    //   year: 2018,
-    //   desc: "Latest certifacte",
-    //   place: "This place",
-    // },
-    // {
-    //   id: 4,
-    //   year: 2017,
-    //   desc: "Latest certifacte",
-    //   place: "This place",
-    // },
-    // {
-    //   id: 5,
-    //   year: 2016,
-    //   desc: "Latest certifacte",
-    //   place: "This place",
-    // },
-  ];
-  
-  let dispNavTop = document.getElementById("navbartop");
-  navbartop.forEach((data) => {
-    dispNavTop.innerHTML += `
-    
-`;
-  });
+let dispNavTop = document.getElementById("navbarTop");
+navbarTop.forEach((data) => {
+  dispNavTop.innerHTML += `
+  <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link" href="${data.link1}$">${data.name1}$</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="${data.link2}$">${data.name2}$</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="${data.link3}$">${data.name3}$</a>
+  </li>
+</ul> `;
+});
+
+
